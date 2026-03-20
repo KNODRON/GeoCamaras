4a35657b53a294c4ec08a040a39b33232515ddbd
KNODRON <juan.jimenezv08@gmail.com>
2026-03-20 11:37:18 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,8 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyBinfL_PpNgWpHZ6cVA25LAkdaE9hDgoAI",
+export const firebaseConfig = {
+  apiKey: "AIzaSyBinfL_PgNpWHZ6cVA25LAkdaE9hDgoAI",
   authDomain: "geocamara-913d6.firebaseapp.com",
   projectId: "geocamara-913d6",
   storageBucket: "geocamara-913d6.firebasestorage.app",
   messagingSenderId: "805498931282",
-  appId: "1:805498931282:web:204dc40a288098292e778b",
-  measurementId: "G-WM6LV8TCJ2"
+  appId: "1:805498931282:web:204dc40a288098292e778b"
 };
