f1e093de72f2bb6c609b8c25e22f4a827f9f6770
KNODRON <juan.jimenezv08@gmail.com>
2026-03-31 14:44:30 -0300

    Update guards.js

--- js/guards.js
+++ js/guards.js
@@ -44,4 +44,17 @@ export function requireRole(expectedRole, onSuccess) {
       window.location.href = "./index.html";
     }
   });
+
+  import { getAuth, onAuthStateChanged } 
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const auth = getAuth();
+
+onAuthStateChanged(auth, (user) => {
+  if (!user) {
+    window.location.href = "login.html";
+  } else {
+    console.log("Acceso permitido:", user.email);
+  }
+});
 }
