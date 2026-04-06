57126f5e4c3cb9abd1b5dc40da1dee4e8ff8635d
KNODRON <juan.jimenezv08@gmail.com>
2026-04-06 18:28:28 -0400

    Update service worker for caching and fetch handling

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,58 @@
+const CACHE_NAME = "georegistro-v1.0.0";
+const APP_ASSETS = [
+  "./",
+  "./index.html",
+  "./login.html",
+  "./operador.html",
+  "./admin.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./js/auth.js",
+  "./js/firebase-config.js",
+  "./js/guards.js",
+  "./js/admin.js",
+  "./js/operador.js"
+];
+
 self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_ASSETS))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keys) => Promise.all(keys.map((key) => caches.delete(key))))
-      .then(() => self.clients.claim())
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
   );
 });
 
-self.addEventListener("fetch", () => {
-  // Desactivado temporalmente en desarrollo
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return (
+        cachedResponse ||
+        fetch(event.request)
+          .then((networkResponse) => {
+            const responseClone = networkResponse.clone();
+            caches.open(CACHE_NAME).then((cache) => {
+              cache.put(event.request, responseClone);
+            });
+            return networkResponse;
+          })
+          .catch(() => caches.match("./index.html"))
+      );
+    })
+  );
 });
